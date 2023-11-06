d21940168d6724a4d6aad0049f28634c62a37f78
Angell <Sa1Lo_xd@vk.cpm>
2023-11-06 19:25:28 +1100

    operator 2

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        //task1();// задача 1
+        //task2();// задача 2
+        //task3();// задача 3
+        //task4();// задача 4
+        //task5();// задача 5
+        //task6();// задача 6
+        //task7();// задача 7
+
+    }
+}
